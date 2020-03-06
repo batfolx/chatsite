73effbe755021d7b5ff4635a1db56ecd5cac5a87
batfolx <victorvelea007@yahoo.com>
2020-03-05 22:39:57 -0500

    current online users work, now need to get server to hold all chat logs

--- server.js
+++ server.js
@@ -5,21 +5,25 @@ var io = require('socket.io')(http);
 
 const users = {}
 
+
 app.use("/static", express.static('./static/'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-  //res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/static/index.html');
 });
 
 
 
 io.on('connection', socket => {
-  console.log("New user!")
   socket.on('new-user', name => {
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
-  })
+    io.emit('update-online-users', users) // broadcast to everyone
+    console.log(`New user connected`)
+    console.log(users)
+    })
+
+
 
   socket.on('send-chat-message', message => { // send-chat-message is name of event, as defined in script.js
     socket.broadcast.emit('chat-message', message) // broadcast.emit sends everyone the message, except for the person that sent message
@@ -30,6 +34,8 @@ io.on('connection', socket => {
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnect', `${users[socket.id]} has disconnected`)
     delete users[socket.id]
+    socket.broadcast.emit('update-online-users', users)
+    console.log("User disconnected")
   })
 });
 
--- static/script.js
+++ static/script.js
@@ -1,38 +1,38 @@
-//const socket = io('http://3.15.38.202:3000') // amazon instance
-const socket = io('http://172.30.5.220:3000') // local
+const socket = io('http://3.15.38.202:3000') // amazon instance
+//const socket = io('http://192.168.1.9:3000') // local
 const messageForm = document.getElementById('send-container')
 const messageContainer = document.getElementById('message-container')
 const messageInput = document.getElementById('message-input')
 const onlineUsers = document.getElementById('online-users-container')
 
 
-//const name = prompt("What is your name?") // user connects and prompts for name
-const name = "Victor"
-appendMessage("You joined!") // tells you you joined chat room
+const name = prompt("What is your name?") // user connects and prompts for name
+//const name = "Victor"
+appendMessage("You joined!", true) // tells you you joined chat room
 socket.emit('new-user', name) // sends a new-user event
 
+socket.on('update-online-users', users => {
+  setOnlineUsers(users)
+  console.log("Updated users!")
+})
 
 socket.on('chat-message', data => {
-  appendMessage(data)
+  appendMessage(data, false)
 })
 
 
 // these are kind of like action listeners, with 'user-connected' being
 // an event that the server has to listen to
 socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
+  appendMessage(`${name} connected`, false)
 })
 
 socket.on('user-disconnect', message => {
-  appendMessage(message)
-})
-
-socket.on('update-online-users', users => {
-  setOnlineUsers(users)
+  appendMessage(message, false)
 })
 
 
-//socket.on('on-disconnect')
+
 
 // the button inside of message form acts as the listener
 messageForm.addEventListener('submit', e => {
@@ -41,14 +41,16 @@ messageForm.addEventListener('submit', e => {
   const data = `${name}: ${message}`
   socket.emit('send-chat-message', data) // use the socket from script.js to send a message event
   messageInput.value = '' //reset message input
-  appendMessage(`You: ${message}`)
+  appendMessage(`You: ${message}`, true)
 })
 
 
 
-function appendMessage(message) {
+function appendMessage(message, self) {
   const messageDiv = document.createElement('div')
   messageDiv.innerText = message
+  if (self) { messageDiv.style.backgroundColor = "white" }
+  else { messageDiv.style.backgroundColor = "lightgray" }
   messageContainer.append(messageDiv)
 }
 
@@ -57,9 +59,10 @@ function setOnlineUsers(users) {
   onlineUsers.innerHtml = ""
   var info = ""
   for (var user in users) {
-    info += `$Online: ${users[user]}\n`
+    let n = users[user]
+    if (name === n) {info += `Online: (You)${n}\n`}
+    else {info += `Online: ${n}\n`}
   }
-  info += "Dummytext\n"
-  onlineUsers.innerHtml = info
+  onlineUsers.innerText = info
 
 }
